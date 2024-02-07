3c70617b871e740b9798d18e7fffd4262747a019
judithgispert <jugispert@gmail.com>
2024-02-07 13:44:17 +0100

    Create class

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/controller/DiceRollController.java
@@ -0,0 +1,10 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/game")
+public class DiceRollController {
+
+}
