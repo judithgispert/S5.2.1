cccdc764b61bf201a79f9dd922116769de102095
judithgispert <jugispert@gmail.com>
2024-02-12 16:58:29 +0100

    Method find by name

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/repository/IPlayerRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/repository/IPlayerRepository.java
@@ -3,5 +3,8 @@ package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.repository;
 import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.domain.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IPlayerRepository extends JpaRepository<Player, Integer> {
+    Optional<Player> findByName(String name);
 }
