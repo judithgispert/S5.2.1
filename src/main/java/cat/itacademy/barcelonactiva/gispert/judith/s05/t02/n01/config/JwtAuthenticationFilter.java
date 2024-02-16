0d991a2ea31784fcc0e659c317109411b3de56fa
judithgispert <jugispert@gmail.com>
2024-02-16 13:17:07 +0100

    Security configuration

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/config/JwtAuthenticationFilter.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/config/JwtAuthenticationFilter.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter { 
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private UserService userService;
 
