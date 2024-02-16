40e22887998f461c97ef5395125f739db2ee1a82
judithgispert <jugispert@gmail.com>
2024-02-16 10:40:26 +0100

    Create response token

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/dto/response/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    private String token;
+}
