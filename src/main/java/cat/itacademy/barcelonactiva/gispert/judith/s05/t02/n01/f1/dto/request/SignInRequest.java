e72ec8558d7fe461705bba582e39b96b6d1b8eb7
judithgispert <jugispert@gmail.com>
2024-02-16 10:39:41 +0100

    Create sign in req

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/dto/request/SignInRequest.java
@@ -0,0 +1,17 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.dto.request;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInRequest {
+    private String email;
+    @Size(min = 8)
+    private String password;
+}
