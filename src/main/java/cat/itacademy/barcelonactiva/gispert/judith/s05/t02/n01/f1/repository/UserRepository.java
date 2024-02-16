587c944e5550e345b2a30de7ffe8786d0e31637d
judithgispert <jugispert@gmail.com>
2024-02-16 13:10:56 +0100

    Create user repository

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.repository;
+
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> finByEmail(String email);
+}
