db087d9597acdedcb24b6ee4157b7aea79d83c71
judithgispert <jugispert@gmail.com>
2024-02-16 09:41:27 +0100

    Use lombok

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/dto/DiceRollDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/f1/dto/DiceRollDTO.java
@@ -1,7 +1,11 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.dto;
 
 import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.f1.methods.DiceRandomNum;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DiceRollDTO {
     private int idDiceRoll;
     private int dice1;
@@ -12,32 +16,6 @@ public class DiceRollDTO {
         this.dice1 = DiceRandomNum.randomNum();
         this.dice2 = DiceRandomNum.randomNum();
         this.resultWin = winGame();
-    }
-
-    public int getIdDiceRoll() {
-        return idDiceRoll;
-    }
-    public int getDice1() {
-        return dice1;
-    }
-    public int getDice2() {
-        return dice2;
-    }
-    public boolean getResultWin() {
-        return resultWin;
-    }
-
-    public void setIdDiceRoll(int idDiceRoll) {
-        this.idDiceRoll = idDiceRoll;
-    }
-    public void setDice1(int dice1) {
-        this.dice1 = dice1;
-    }
-    public void setDice2(int dice2) {
-        this.dice2 = dice2;
-    }
-    public void setResultWin(boolean resultWin) {
-        this.resultWin = resultWin;
     }
 
     public boolean winGame(){
