7154cea63c56eaf3ed4d74623a4e6dc01db3dce5
judithgispert <jugispert@gmail.com>
2024-02-17 20:12:47 +0100

    Fixing name method errors

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/repository/UserRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> finByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
