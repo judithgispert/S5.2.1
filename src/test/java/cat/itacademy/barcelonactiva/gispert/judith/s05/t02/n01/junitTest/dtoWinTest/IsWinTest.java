a2875792e417cda0c0b1873fa70922676ab99763
judithgispert <jugispert@gmail.com>
2024-02-21 11:10:38 +0100

    Test method isWin (playerDTO)

---
+++ src/test/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/junitTest/dtoWinTest/IsWinTest.java
@@ -0,0 +1,20 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.junitTest.dtoWinTest;
+
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.dto.DiceRollDTO;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IsWinTest {
+    @Test
+    void isWinTest (){
+        DiceRollDTO diceRollDTO = new DiceRollDTO(1,6);
+        Assertions.assertEquals(7, diceRollDTO.getDice1() + diceRollDTO.getDice2());
+    }
+
+    @Test
+    void isLoseTest(){
+        DiceRollDTO diceRollDTO = new DiceRollDTO(1,4);
+        Assertions.assertTrue(diceRollDTO.getDice1() + diceRollDTO.getDice2() != 7);
+    }
+}
