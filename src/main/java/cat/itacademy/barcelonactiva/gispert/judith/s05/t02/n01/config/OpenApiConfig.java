6bdc234a9f409fed14521bdd45c9a6f93f19ab33
judithgispert <jugispert@gmail.com>
2024-02-21 12:19:17 +0100

    Create open api config

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Dice Roll Game API Service")
+                        .description("Roll the dice, you win if the result of their sum is 7.")
+                        .version("1.0").contact(new Contact().name("Judith Gispert")
+                                .email("gispertjudith@gmail.com")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
